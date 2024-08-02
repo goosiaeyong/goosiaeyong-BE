59bacd1ced62bb0f3c232e4cf511a22f371322d7
de123456sdf <cdaeun95@gmail.com>
2024-08-02 12:12:41 +0900

    add: 환경 설정

---
+++ src/main/java/good/k_html/KHtmlApplication.java
@@ -0,0 +1,13 @@
+package good.k_html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KHtmlApplication.class, args);
+    }
+
+}
